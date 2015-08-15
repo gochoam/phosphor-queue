8a9db73e0f2be2164547b6cb3b7fd82d297b55fa
S. Chris Colbert <sccolbert@gmail.com>
2015-08-15 19:12:32 -0400

    update to dts-generator

--- scripts/dtsgenerate.js
+++ scripts/dtsgenerate.js
@@ -1,4 +1,8 @@
-require('dts-bundle').bundle({
+require('dts-generator').generate({
   name: 'phosphor-queue',
-  main: 'lib/index.d.ts',
+  main: 'phosphor-queue/index',
+  baseDir: 'lib',
+  files: ['index.d.ts'],
+  out: 'lib/phosphor-queue.d.ts',
+  target: 1 // ts.ScriptTarget.ES5
 });
